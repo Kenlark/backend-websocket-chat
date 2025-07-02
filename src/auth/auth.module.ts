11ca84683aa4a324738bf213cd5e7d9ab55782b6
kenlark <kenzokerachi@hotmail.fr>
2025-07-02 10:59:43 +0200

    fix: fix authentication module to use ConfigService for JWT secret and add @types/bcrypt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,16 +4,26 @@ import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: process.env.JWT,
-      signOptions: { expiresIn: '1h' },
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get<string>('JWT'),
+        signOptions: { expiresIn: '1h' },
+      }),
     }),
     UsersModule,
   ],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule {
+  constructor() {
+    console.log('JWT secret:', process.env.JWT);
+  }
+}
